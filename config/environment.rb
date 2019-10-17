ab96f7fd0541a90f454c1eaafbb68ee324148dae
TJBender <tiana.bender01@gmail.com>
2019-10-17 14:38:20 -0400

    Done.

--- config/environment.rb
+++ config/environment.rb
@@ -1,14 +1,17 @@
+# config/environment.rb
 require 'rake'
 require 'active_record'
 require 'yaml/store'
 require 'ostruct'
 require 'date'
-
-
-require 'bundler/setup'
-Bundler.require
-
+# require 'sinatra/activerecord/rake'
 
 # put the code to connect to the database here
+ActiveRecord::Base.establish_connection(
+  :adapter => "sqlite3",
+  :database => "db/artists.sqlite"
+)
+require 'bundler/setup'
+Bundler.require
 
 require_relative "../artist.rb"
